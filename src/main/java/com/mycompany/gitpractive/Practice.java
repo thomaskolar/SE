9dcdb04377460d2ed8ba68780edf305714db2223
Kolar <thomas.kolar@go.tarleton.edu>
2021-09-16 08:50:07 -0500

    Second Commit

--- src/main/java/com/mycompany/gitpractive/Practice.java
+++ src/main/java/com/mycompany/gitpractive/Practice.java
@@ -5,5 +5,8 @@ package com.mycompany.gitpractive;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("first commit");
+        System.out.println("second commit");
+        System.out.println("third commit");
+        System.out.println("");
     }
 }
